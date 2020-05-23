cad08bb2a6209f986d7f5ad8e1bba0f35e9b0dd0
5HT-2A <30508313+cakehonolulu@users.noreply.github.com>
2020-05-23 18:27:12 +0200

    Memory map

--- nucleus/kernel.c
+++ nucleus/kernel.c
@@ -1,5 +1,49 @@
 #include "gdt.h"
 #include <stddef.h>
+#include <stdint.h>
+
+#define MEMORY_MAP 0x9000
+
+enum bios_memmap_type
+{
+    MEMORY_TYPE_USABLE = 1,
+    MEMORY_TYPE_RESERVED = 2,
+    MEMORY_TYPE_ACPI_RECLAMIABLE = 3,
+    MEMORY_TYPE_ACPI_NON_VOLATILE = 4,
+    MEMORY_TYPE_BAD_AREA = 5,
+};
+
+struct bios_memmap_entry
+{
+    uint64_t base;
+    uint64_t size;
+    uint32_t type;
+    uint32_t attrib;
+};
+
+void mem_init(void)
+{
+    static const char* strtype[] = {
+        "Unknown",
+        "Usable",
+        "Reserved",
+        "Reclaimable",
+        "Non-Volatile",
+        "Bad Area"
+    };
+
+    uint32_t len = *(uint32_t*)MEMORY_MAP;
+    const struct bios_memmap_entry* entry = (const struct bios_memmap_entry*)(MEMORY_MAP + 8);
+
+    printk("Memory map report: %d entries\n", len);
+    while (len-- != 0)
+    {
+        const char* t = entry->type <= 5 ? strtype[entry->type] : strtype[0];
+        printk("Start: 0x%x; End: 0x%x; Size: %d bytes; Type: %s\n",
+            entry->base, entry->base + entry->size, entry->size, t);
+        ++entry;
+    }
+}
 
 void _start(unsigned int ferrum_signature, unsigned int ferrum_low_mem)
 {
@@ -44,6 +88,8 @@ void _start(unsigned int ferrum_signature, unsigned int ferrum_low_mem)
     printk("\n");
     
     printkok("Booted to kernel mode!");
+
+    mem_init();
 
 	if (i386_gdt_install() == 1)
 	{
