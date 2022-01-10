2d3ee0d1efede558226276be2027568430122bb7
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-01-10 22:28:03 +0100

    initium/boot1->fat16: Stage 2 Protected Mode from C

--- bootloader/x86/stage2/stage2.c
+++ bootloader/x86/stage2/stage2.c
@@ -1,11 +1,18 @@
+// note this example will always write to the top
+// line of the screen
+void write_string( int colour, const char *string )
+{
+    volatile char *video = (volatile char*)0xB8000;
+    while( *string != 0 )
+    {
+        *video++ = *string++;
+        *video++ = colour;
+    }
+}
+
 int stage2()
 {
-	asm volatile("	mov $0xb800, %ax\n\t"
-				" 	mov %ax, %es\n\t"
-				"	mov $0x4020, %ax\n\t"
-				"	mov $2000, %cx\n\t"
-				"	xor %di, %di\n\t"
-				"	rep stosw");
-	
+	write_string(0x0F, "Hello from Stage 2 - 32 bit C");
+
 	asm volatile("cli; hlt");
 }
