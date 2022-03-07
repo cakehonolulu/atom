3a9890f14cc9ab3792e2800f36ce8c31dfd38e5d
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-07 18:07:05 +0100

    bl-fat-stage2 -> textmode: Header function definitions

--- bootloader/x86/fat/stage2/include/textmode.h
+++ bootloader/x86/fat/stage2/include/textmode.h
@@ -6,5 +6,10 @@
 #define CUR_START_REG	0x0A
 #define CUR_LOC_HI_REG	0x0E
 #define CUR_LOC_LO_REG	0x0F
+
 #define CRTC_ADDR_REG	0x3D4
 #define CRTC_DATA_REG	0x3D5
+
+/* Functions */
+uint16_t get_cur_pos();
+void disable_cur();
