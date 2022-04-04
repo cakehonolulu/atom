89099ab3433efab11d842e50f1d4042048f5b3a0
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-04-04 11:50:31 +0200

    [bl - i386 - fat] STAGE2: Implement the rest of INx and OUTx functions

--- bootloader/i386/fat/stage2/include/io.h
+++ bootloader/i386/fat/stage2/include/io.h
@@ -2,5 +2,12 @@
 
 /* Function definitions */
 
+// INx
 uint8_t inb(uint16_t m_address);
+uint16_t inw(uint16_t m_address);
+uint32_t inl(uint16_t m_address);
+
+// OUTx
 void outb(uint16_t m_address, uint8_t m_data);
+void outw(uint16_t m_address, uint16_t m_data);
+void outl(uint16_t m_address, uint32_t m_data);
--- bootloader/i386/fat/stage2/io.c
+++ bootloader/i386/fat/stage2/io.c
@@ -31,6 +31,62 @@ uint8_t inb(uint16_t m_address)
 }
 
 /*
+	inw
+
+	Function information:
+	Returns a word (2 bytes) from the Port I/O
+
+	Parameters:
+	m_address -> 16-bit immediate address
+	containing the m_address to read from
+
+	Return:
+	Word read at m_address
+*/
+uint16_t inw(uint16_t m_address)
+{
+	// Value readed
+    uint16_t m_value;
+
+	/*
+		=a specifies where it should put the return value
+		Nd specifies %dx register or 16-bit immediate addr. for inw
+	*/
+    __asm__ __volatile__ ("inw %1, %0"
+    					: "=a" (m_value)
+    					: "Nd" (m_address));
+    return m_value;
+}
+
+/*
+	inl
+
+	Function information:
+	Returns a dword (4 bytes) from the Port I/O
+
+	Parameters:
+	m_address -> 16-bit immediate address
+	containing the m_address to read from
+
+	Return:
+	Dword read at m_address
+*/
+uint32_t inl(uint16_t m_address)
+{
+	// Value readed
+    uint32_t m_value;
+
+	/*
+		=a specifies where it should put the return value
+		d specifies %edx register or 32-bit immediate addr. for inl
+	*/
+    __asm__ __volatile__ ("inl %1, %0"
+    					: "=a" (m_value)
+    					: "d" (m_address));
+    return m_value;
+}
+
+/*
 	outb
 
 	Function information:
@@ -54,4 +110,56 @@ void outb(uint16_t m_address, uint8_t m_data)
 	__asm__ __volatile__ ("outb %0, %1"
 						:
 						: "a" (m_data), "Nd" (m_address));
+}
+
+/*
+	outw
+
+	Function information:
+	Sends a byte to the Port I/O
+
+	Parameters:
+	m_address -> 16-bit immediate address
+	containing the port to write to
+
+	m_data -> 16-bit value to write
+
+	Return:
+	none
+*/
+void outw(uint16_t m_address, uint16_t m_data)
+{
+	/*
+		a specifies the 'a' register
+		Nd specifies %dx register or 16-bit immediate addr. for outw
+	*/
+    __asm__ __volatile__ ("outw %0, %1"
+    					:
+    					: "a" (m_data), "d" (m_address));
+}
+
+/*
+	outl
+
+	Function information:
+	Sends a byte to the Port I/O
+
+	Parameters:
+	m_address -> 16-bit immediate address
+	containing the port to write to
+
+	m_data -> 16-bit value to write
+
+	Return:
+	none
+*/
+void outl(uint16_t m_address, uint32_t m_data)
+{
+	/*
+		a specifies the 'a' register
+		Nd specifies %dx register or 32-bit immediate addr. for outw
+	*/
+    __asm__ __volatile__ ("outl %0, %1"
+    					:
+    					: "a" (m_data), "d" (m_address));
 }
