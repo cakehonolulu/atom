76c7f573c0e073e769f77eaac333c2d4834ef826
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-28 22:57:22 +0200

    fat16: BPB Defines

--- bootloader/i386/fat/stage2/include/fat16.h
+++ bootloader/i386/fat/stage2/include/fat16.h
@@ -1,1 +1,17 @@
 #include <stdint.h>
+
+/* Defines */
+
+#define BPB_JMPNOPS 0x00
+#define BPB_OEMSTRN 0x03
+#define BPB_BYTESPS 0x0B
+#define BPB_SECSTPC 0x0D
+#define BPB_RESVSEC 0x0E
+#define BPB_NUMFATS 0x10
+#define BPB_TOTALST 0x11
+#define BPB_MEDIATP 0x15
+#define BPB_FATSIZE 0x16
+#define BPB_SECTPTR 0x18
+#define BPB_NUMHEAD 0x1A
+#define BPB_HIDSECT 0x1C
+#define BPB_TOTSECT 0x20
