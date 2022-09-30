0f42bd97ba06a2fef7b9eff55d102f6b2af3c59d
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-30 20:57:47 +0200

    fat16: Iterate the entire root dir entries sector by sector in order to find a file

--- bootloader/i386/fat/stage2/fat16.c
+++ bootloader/i386/fat/stage2/fat16.c
@@ -45,28 +45,48 @@ void fat16_file_info(fat16_entry_t *fat16_entry) {
 
 void fat16_parse(uint8_t *m_bpb)
 {
+    bool m_cond = true;
+
+    int i = 0;
+
+    uint8_t m_sect[512];
+
+    uint32_t root_directory_start, root_directory_size, data_sector_start;
+
     fat16_bpb_t* fat16 = (fat16_bpb_t*)m_bpb;
 
     puts("Calculating FAT16 information...\n");
 
-    uint32_t root_directory_start = (fat16->reserved_sector_count) + (fat16->table_count * fat16->table_size_16);
+    root_directory_start = (fat16->reserved_sector_count) + (fat16->table_count * fat16->table_size_16);
 
-    uint32_t root_directory_size = (((fat16->root_entry_count * 32) + (fat16->bytes_per_sector - 1)) / fat16->bytes_per_sector);
+    root_directory_size = (((fat16->root_entry_count * 32) + (fat16->bytes_per_sector - 1)) / fat16->bytes_per_sector);
 
-    uint32_t data_sector_start = root_directory_start + root_directory_size;
+    data_sector_start = root_directory_start + root_directory_size;
 
     puts("Root Directory Start: %d (LBA Sector)\n", root_directory_start);
     puts("Root Directory Size: %d (Sectors)\n", root_directory_size);
 
     puts("Data Sector Start: %d (LBA Sector)\n", data_sector_start);
 
-    uint8_t m_sect[512];
-
-    // Read one entry for now
-    for (int i = 0; i < fat16->root_entry_count; i++)
+    do
     {
-        atapio24_read((uint32_t *) &m_sect, root_directory_start, 1);
-        //memcmp
-    }
+        atapio24_read((uint32_t *) m_sect, root_directory_start, 1);
+        
+        if (memcmp("STAGE2     ", ((char *) (((fat16_entry_t *) m_sect)->filename)), 11) == 0)
+        {
+            puts("Found STAGE2!\n");
+            m_cond = false;
+        }
+
+        if (i >= (fat16->root_entry_count - 1))
+        {
+            m_cond = false;
+        }
+        else
+        {
+            i++;
+        }
+    
+    } while (m_cond == true);
 }
 
