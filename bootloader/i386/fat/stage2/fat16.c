bde4a630e29a18019e53a2dc0b5a91ca6717677b
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-30 20:33:49 +0200

    fat16: Correctly parse the root dir

--- bootloader/i386/fat/stage2/fat16.c
+++ bootloader/i386/fat/stage2/fat16.c
@@ -47,42 +47,26 @@ void fat16_parse(uint8_t *m_bpb)
 {
     fat16_bpb_t* fat16 = (fat16_bpb_t*)m_bpb;
 
-    uint32_t rootdir_size;
+    puts("Calculating FAT16 information...\n");
 
-    puts("BPB OEM name: ");
+    uint32_t root_directory_start = (fat16->reserved_sector_count) + (fat16->table_count * fat16->table_size_16);
 
-    for (int i = 0; i <= 8; i++)
-    {
-        puts("%c", fat16->oem_name[i]);
-    }   
+    uint32_t root_directory_size = (((fat16->root_entry_count * 32) + (fat16->bytes_per_sector - 1)) / fat16->bytes_per_sector);
 
-    puts("\nBPB Sectors Long: %d", fat16->total_sectors_32);
+    uint32_t data_sector_start = root_directory_start + root_directory_size;
 
-    puts("\nBPB Filesystem: ");
+    puts("Root Directory Start: %d (LBA Sector)\n", root_directory_start);
+    puts("Root Directory Size: %d (Sectors)\n", root_directory_size);
 
-    for (int i = 0; i < BPB_FSTYPELEN; i++)
-    {
-        puts("%c", m_bpb[BPB_FILESYS + i]);
-    }
-
-    puts("\nBPB Media Type: 0x%X\n", fat16->media_type);
-
-    rootdir_size = (((fat16->root_entry_count * 32) + (fat16->bytes_per_sector - 1)) / fat16->bytes_per_sector);
-
-    puts("Root Directory Size: %d (Sectors)\n", rootdir_size);
+    puts("Data Sector Start: %d (LBA Sector)\n", data_sector_start);
 
     uint8_t m_sect[512];
-    uint32_t first_data_sector;
-
-    first_data_sector = (fat16->table_count * fat16->table_size_16) + fat16->reserved_sector_count + fat16->hidden_sector_count;
-
-    puts("First Data Sector: %d\n", first_data_sector);
-
-    puts("Number of FAT Tables: %d\n", fat16->table_count);
-    puts("Sectors per FAT: %d\n", fat16->table_size_16);
 
     // Read one entry for now
-    atapio24_read((uint32_t *) &m_sect, first_data_sector, 1);
-    fat16_file_info((fat16_entry_t *) &m_sect);
+    for (int i = 0; i < fat16->root_entry_count; i++)
+    {
+        atapio24_read((uint32_t *) &m_sect, root_directory_start, 1);
+        //memcmp
+    }
 }
 
