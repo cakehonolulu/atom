35de9c27806a7704bb649eb9aa2ed72ff5936db3
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-10-01 20:57:10 +0200

    fat16: Uni-cluster file loading works

--- bootloader/i386/fat/stage2/fat16.c
+++ bootloader/i386/fat/stage2/fat16.c
@@ -61,14 +61,14 @@ void fat16_parse(uint8_t *m_bpb)
 
     root_directory_size = (((fat16->root_entry_count * 32) + (fat16->bytes_per_sector - 1)) / fat16->bytes_per_sector);
 
-    data_sector_start = root_directory_start + root_directory_size;
+    data_sector_start = (root_directory_start + root_directory_size);
 
     puts("# of Root Directory Entries: %d\n", fat16->root_entry_count);
 
-    puts("Root Directory Start: %d (LBA Sector)\n", root_directory_start);
-    puts("Root Directory Size: %d (Sectors)\n", root_directory_size);
+    puts("Root Directory Start: %d (LBA Sector), Offset: 0x%X\n", root_directory_start, (512 * root_directory_start));
+    puts("Root Directory Size: %d (Sectors) (%d bytes)\n", root_directory_size, (root_directory_size * 512));
 
-    puts("Data Sector Start: %d (LBA Sector)\n", data_sector_start);
+    puts("Data Sector Start: %d (LBA Sector), Offset: 0x%X\n", data_sector_start, (512 * data_sector_start));
 
     do
     {
@@ -76,7 +76,18 @@ void fat16_parse(uint8_t *m_bpb)
         
         if (memcmp("TEST       ", ((char *) (((fat16_entry_t *) m_sect)->filename)), 11) == 0)
         {
-            puts("Found TEST!\n");
+            puts("Found TEST @ %d (LBA), FAT Cluster: %d\n", (((data_sector_start) + ((((fat16_entry_t *) m_sect)->starting_cluster) - 2))), (((fat16_entry_t *) m_sect)->starting_cluster));
+
+            puts("Reading from 0x%X...\n", ((((data_sector_start) + ((((fat16_entry_t *) m_sect)->starting_cluster) - 2)) )) * 512 );
+
+
+            atapio24_read((uint32_t *) m_sect, (data_sector_start + ((((fat16_entry_t *) m_sect)->starting_cluster) - 2)), 1);
+
+            puts("File read successful, jumping to entrypoint @ 0x%X\n", &m_sect[0]);
+
+
+            ((void(*)())m_sect)();
+
             m_cond = false;
         }
 
