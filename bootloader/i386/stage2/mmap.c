dbdbbdf81ad7b10b416eafbaf07a9bf86d7404a2
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-10-10 16:16:17 +0200

    stage2/mmap: Fix display of mmap

--- bootloader/i386/stage2/mmap.c
+++ bootloader/i386/stage2/mmap.c
@@ -1,30 +1,4 @@
-#include <stddef.h>
-#include <stdint.h>
-
-#ifdef EXT2
-#define MEMORY_MAP_LOCATION (0x00000500)
-#else
-#ifdef FAT16
-#define MEMORY_MAP_LOCATION (0x00000500)
-#endif
-#endif
-
-enum bios_memmap_type
-{
-    MEMORY_TYPE_USABLE = 1,
-    MEMORY_TYPE_RESERVED = 2,
-    MEMORY_TYPE_ACPI_RECLAMIABLE = 3,
-    MEMORY_TYPE_ACPI_NON_VOLATILE = 4,
-    MEMORY_TYPE_BAD_AREA = 5,
-};
-
-struct bios_memmap_entry
-{
-    uint64_t e820_entry_base_address;
-    uint64_t e820_entry_size;
-    uint32_t e820_entry_type;
-    uint32_t e820_entry_attribute;
-};
+#include <mmap.h>
 
 static const char* e820_type_string[] = {
     "Unknown",
@@ -35,21 +9,15 @@ static const char* e820_type_string[] = {
     "Bad Memory"
 };
 
-struct e820_mmap_available_entries {
-   uintptr_t base_address;
-   uintptr_t end_address;
-};  
-
 void parse_mmap()
 {
     size_t e820_mmap_total_entries_for_counting = *(uintptr_t*)MEMORY_MAP_LOCATION;
     const struct bios_memmap_entry* entry = (const struct bios_memmap_entry*)(MEMORY_MAP_LOCATION + 8);
 
-    uintptr_t e820_entry_base_address = 0;
-    uintptr_t e820_entry_end_address = 0;
-    size_t e820_entry_size = 0;
+    uintptr_t e820_entry_base_address = 0, e820_entry_end_address = 0;
+    size_t e820_entry_size = 0, e820_counted_entries = 0, e820_mmap_total_entries;
 
-    size_t e820_counted_entries = 0;
+    const char* t;
 
     // Count total available entries
     while (e820_mmap_total_entries_for_counting-- != 0)
@@ -57,10 +25,11 @@ void parse_mmap()
         e820_counted_entries++;
     }
 
+    puts("mmap: %d entries\n", e820_counted_entries);
 
-    puts("Memory map report: %d entries\n", e820_counted_entries);
+    e820_mmap_total_entries = *(uintptr_t*)MEMORY_MAP_LOCATION;
 
-    size_t e820_mmap_total_entries = *(uintptr_t*)MEMORY_MAP_LOCATION;
+    puts("Start     | End       | Size      | Type\n");
 
     while (--e820_mmap_total_entries != 0)
     {
@@ -69,15 +38,10 @@ void parse_mmap()
         e820_entry_base_address = entry->e820_entry_base_address & 0xFFFFFFFF;
         e820_entry_end_address = e820_entry_base_address + e820_entry_size;
 
-        const char* t = entry->e820_entry_type <= MEMORY_TYPE_BAD_AREA ?
-        e820_type_string[entry->e820_entry_type] : e820_type_string[0];
+        t = entry->e820_entry_type <= MEMORY_TYPE_BAD_AREA ? e820_type_string[entry->e820_entry_type] : e820_type_string[0];
 
-		if (((e820_entry_size / 1024) / 1024) != 0) {
-			puts("Start: 0x%x; End: 0x%x Size: %d bytes (%d MB); Type: %s\n", e820_entry_base_address, e820_entry_end_address, e820_entry_size, ((e820_entry_size / 1024) / 1024), t);
-		} else {
-			puts("Start: 0x%x; End: 0x%x Size: %d bytes; Type: %s\n", e820_entry_base_address, e820_entry_end_address, e820_entry_size, t);
-		}
+		puts("0x%08x  0x%08x  0x%08x  %s\n", e820_entry_base_address, e820_entry_end_address, e820_entry_size, t);
 
-        ++entry;
+        entry++;
     }
 }
